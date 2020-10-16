40b18043fe21d0f3fe3247192e006f38910d64de
Amine EL HAOUNI <amine@penni.io>
2020-10-17 01:20:30 +0200

    Implementation of sum function

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,7 +1,28 @@
 import { expect } from 'chai';
+import { sum } from '../src/index'
 
-describe('Test Suite', () => {
-    it('Atomic test title', () => {
-        expect(1).to.equal(1);
+describe('Test function sum', () => {
+    it('Test adding two positif number', () => {
+        expect(sum(3, 4)).to.equal(7);
+    });
+
+    it('Test adding two negatif number', () => {
+        expect(sum(-3, -4)).to.equal(-7);
+    });
+
+    it('Test adding two 0 number', () => {
+        expect(sum(0, 0)).to.equal(0);
+    });
+
+    it('Test adding positf and negatif number', () => {
+        expect(sum(-3, 4)).to.equal(1);
+    });
+
+    it('Test adding 0 and positif number', () => {
+        expect(sum(0, 4)).to.equal(4);
+    });
+
+    it('Test adding 0 and negatif number', () => {
+        expect(sum(0, -4)).to.equal(-4);
     });
 });
