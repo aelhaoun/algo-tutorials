b022d96595b5374fd6faf1a93490ddc607803d0e
Amine EL HAOUNI <amine@penni.io>
2020-10-18 15:14:24 +0200

    Preparation for Lesson 3

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,28 +1,32 @@
 import { expect } from 'chai';
-import { sum } from '../src/index'
+import { findMax, reverseArray } from '../src/index'
 
-describe('Test function sum', () => {
-    it('Test adding two positif number', () => {
-        expect(sum(3, 4)).to.equal(7);
+describe('Test fin Max in array', () => {
+    it('Test with positif positif number', () => {
+        expect(findMax([3, 0, 4, 2, 6, 1])).to.equal(6);
     });
 
-    it('Test adding two negatif number', () => {
-        expect(sum(-3, -4)).to.equal(-7);
+    it('Test with negatif positif number', () => {
+        expect(findMax([-3, -4, 0, -2, -6, -1])).to.equal(-1);
+    });
+});
+
+describe('Test reverse array', () => {
+    it('Test reverse array with elements', () => {
+        expect(reverseArray([3, 0, 4, 2, 6, 1])).to.equal([1, 0, 4, 2, 6, 1]);
     });
 
-    it('Test adding two 0 number', () => {
-        expect(sum(0, 0)).to.equal(0);
+    it('Test reverse empty array', () => {
+        expect(reverseArray([])).to.equal([]);
+    });
+});
+
+describe('Test filter even number in array', () => {
+    it('Test filter even numbers in array with elements', () => {
+        expect(reverseArray([3, 0, 4, 2, 6, 1])).to.equal([1, 0, 4, 2, 6, 1]);
     });
 
-    it('Test adding positf and negatif number', () => {
-        expect(sum(-3, 4)).to.equal(1);
-    });
-
-    it('Test adding 0 and positif number', () => {
-        expect(sum(0, 4)).to.equal(4);
-    });
-
-    it('Test adding 0 and negatif number', () => {
-        expect(sum(0, -4)).to.equal(-4);
+    it('Test filter even numbers in empty array', () => {
+        expect(reverseArray([])).to.equal([]);
     });
 });
