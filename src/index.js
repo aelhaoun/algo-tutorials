0475c94fd810d69af1eadb2cec659d0c361120de
Amine EL HAOUNI <amine@penni.io>
2020-10-19 00:01:06 +0200

    insertionSort

--- src/index.js
+++ src/index.js
@@ -1,33 +1,89 @@
+import fs from 'fs';
+import { insertionSort } from './sort.js';
 
-// findMax [3, 0, 4, 2, 6, 1] ===> 6;
-export function findMax(array) {
-    let max = array[0];
-    for(const element of array) {
-        if (element > max) {
-            max = element;
-        }
-    }
-    return max;
+function appendToFile(file, text) {
+    
+    fs.open(file, 'a', 666, function (e, id) {
+        fs.write(id, text + "\n", null, 'utf8', function () {
+            fs.close(id, function () {
+                // console.log('file is updated');
+            });
+        });
+    });
 }
-
-// reverseArray [3, 0, 4, 2, 6, 1] ===> [1, 0, 4, 2, 6, 1]
-export function reverseArray(array) {
-    const result = [];
-    for(let i = array.length -1; i >= 0; i--) {
-        const element = array[i];
-        result.push(element);
-    }
-    return result;
-}
-
-// filterEven [3, 0, 4, 2, 6, 1] ===> [0, 4, 2, 6]
-export function filterEven(array) {
-    const result = [];
+function avgArray(array) {
+    let sum = 0;
     for (const element of array) {
-        if (element % 2 === 0) {
-            result.push(element);
-        }
+        sum += element;
     }
-    return result;
+    return sum / array.length;
 }
 
+
+function generateRanomArray(length) {
+    const randomArray = []
+    for (let i = 0; i < length; i++) {
+        randomArray.push(Math.random())
+    }
+    return randomArray
+}
+
+function generateWorstCase(length) {
+    const array = []
+    for (let i = 0; i < length; i++) {
+        array.unshift(i);
+    }
+    return array
+}
+
+function generateBestCase(length) {
+    const array = []
+    for (let i = 0; i < length; i++) {
+        array.push(i);
+    }
+    return array
+}
+
+const lengthList = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300];
+const REPETITON = 500;
+for (const length of lengthList) {
+    let results = [];
+    console.log(`-----------------Random ${length}------------------`)
+    for (let i = 0; i < REPETITON; i++) {
+        let generateArray = generateRanomArray(length);
+        let hrstart = process.hrtime();
+        insertionSort(generateArray);
+        let hrend = process.hrtime(hrstart);
+        results.push(hrend[1]);
+    }
+    let avgTime = avgArray(results) / 1000000;
+    appendToFile('random.txt', `${length} ${avgTime}`)
+}
+
+for (const length of lengthList) {
+    console.log(`-----------------worse case ${length}------------------`)
+    let results = [];
+    for (let i = 0; i < REPETITON; i++) {
+        let generateArray = generateWorstCase(length);
+        let hrstart = process.hrtime();
+        insertionSort(generateArray);
+        let hrend = process.hrtime(hrstart);
+        results.push(hrend[1]);
+    }
+    let avgTime = avgArray(results) / 1000000;
+    appendToFile('worst_case.txt', `${length} ${avgTime}`)
+}
+
+for (const length of lengthList) {
+    console.log(`-----------------best case ${length}------------------`)
+    let results = [];
+    for (let i = 0; i < REPETITON; i++) {
+        let generateArray = generateBestCase(length);
+        let hrstart = process.hrtime();
+        insertionSort(generateArray);
+        let hrend = process.hrtime(hrstart);
+        results.push(hrend[1]);
+    }
+    let avgTime = avgArray(results) / 1000000;
+    appendToFile('best_case.txt', `${length} ${avgTime}`)
+}
---
+++ src/sort.js
@@ -0,0 +1,14 @@
+function swap(array, i, j) {
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+}
+
+export function insertionSort(array) {
+    for (let i = 1; i < array.length; i++) {
+        for (let j = i; j > 0 && array[j] < array[j - 1]; j--) {
+            swap(array, j, j - 1);
+        }
+    }
+    return array;
+}
