9f42b721f5ec30721490c550ae2a1cd2261732ba
Amine EL HAOUNI <amine@penni.io>
2020-10-18 15:56:17 +0200

    add findMax, reverse array and filter Even

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
 
+// findMax [3, 0, 4, 2, 6, 1] ===> 6;
+export function findMax(array) {
+    let max = array[0];
+    for(const element of array) {
+        if (element > max) {
+            max = element;
+        }
+    }
+    return max;
+}
+
+// reverseArray [3, 0, 4, 2, 6, 1] ===> [1, 0, 4, 2, 6, 1]
+export function reverseArray(array) {
+    const result = [];
+    for(let i = array.length -1; i >= 0; i--) {
+        const element = array[i];
+        result.push(element);
+    }
+    return result;
+}
+
+// filterEven [3, 0, 4, 2, 6, 1] ===> [0, 4, 2, 6]
+export function filterEven(array) {
+    const result = [];
+    for (const element of array) {
+        if (element % 2 === 0) {
+            result.push(element);
+        }
+    }
+    return result;
+}
+
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import { findMax, reverseArray } from '../src/index'
+import { findMax, reverseArray, filterEven } from '../src/index'
 
 describe('Test fin Max in array', () => {
     it('Test with positif positif number', () => {
@@ -7,26 +7,26 @@ describe('Test fin Max in array', () => {
     });
 
     it('Test with negatif positif number', () => {
-        expect(findMax([-3, -4, 0, -2, -6, -1])).to.equal(-1);
+        expect(findMax([-3, -4, 0, -2, -6, -1])).to.equal(0);
     });
 });
 
 describe('Test reverse array', () => {
     it('Test reverse array with elements', () => {
-        expect(reverseArray([3, 0, 4, 2, 6, 1])).to.equal([1, 0, 4, 2, 6, 1]);
+        expect(reverseArray([3, 0, 4, 2, 6, 1])).to.eql([1, 6, 2, 4, 0, 3]);
     });
 
     it('Test reverse empty array', () => {
-        expect(reverseArray([])).to.equal([]);
+        expect(reverseArray([])).to.eql([]);
     });
 });
 
 describe('Test filter even number in array', () => {
     it('Test filter even numbers in array with elements', () => {
-        expect(reverseArray([3, 0, 4, 2, 6, 1])).to.equal([1, 0, 4, 2, 6, 1]);
+        expect(filterEven([3, 0, 4, 2, 6, 1])).to.eql([0, 4, 2, 6]);
     });
 
     it('Test filter even numbers in empty array', () => {
-        expect(reverseArray([])).to.equal([]);
+        expect(filterEven([])).to.eql([]);
     });
 });
